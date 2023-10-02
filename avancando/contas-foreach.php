6b2f4ec65f86c4d31e4dba00d71e63303fc557e2
DaniWeos <danielweoswork@gmail.com>
2023-10-02 17:54:03 -0300

    Organize

--- avancando/contas-foreach.php
+++ avancando/contas-foreach.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -14,31 +16,6 @@ $contasCorrentes = [
         'saldo' => 100
     ]
 ];
-
-function exibeMensagem (string $mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar (array $conta, float $valorADepositar): array
-{
-    if ($valorADepositar > 0){
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-    return $conta;
-}
-
-function sacar (array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Saldo em conta indisponível");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
 
 $contasCorrentes ['123.456.789-10'] = sacar(
     $contasCorrentes  ['123.456.789-10'], 
