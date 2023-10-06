7388c97be8178d510e8fe8909c2a34abdc51fe52
DaniWeos <danielweoswork@gmail.com>
2023-10-05 21:49:11 -0300

    regex e expressao regulares

--- desafios/regex.php
+++ desafios/regex.php
@@ -3,8 +3,8 @@
 $telefones = ['(24) 99999 - 9999', '(21) 99999 - 9999', '(24) 2222 - 2222'];
 
 foreach ($telefones as $telefone) {
-    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
-    $telefoneValido = preg_match(
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/'; //expressão regular para mapear um número telefonico
+    $telefoneValido = preg_match( //verificar se a expressao deu algum match
         $regex,
         $telefone,
         $correspondencia
@@ -16,7 +16,7 @@ foreach ($telefones as $telefone) {
         echo 'Telefone inválido'. PHP_EOL;
     }
 
-    echo preg_replace(
+    echo preg_replace( //substituir grupos de uma exReg
         $regex,
         '(XX) \2',
         $telefone
