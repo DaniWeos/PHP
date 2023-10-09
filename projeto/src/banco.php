bdc406cb0f4d14275c8f21f80b7a9a4dfe11791c
DaniWeos <danielweoswork@gmail.com>
2023-10-09 18:38:31 -0300

    Funcionario e Endereco

--- projeto/src/banco.php
+++ projeto/src/banco.php
@@ -1,7 +1,27 @@
 <?php
 
 require_once 'D:\Prog\PHP\projeto\src\Conta.php';
+require_once 'D:\Prog\PHP\projeto\src\Endereco.php';
+require_once 'D:\Prog\PHP\projeto\src\Titular.php';
+require_once 'D:\Prog\PHP\projeto\src\CPF.php';
 
-$primeiraConta = new Conta();
+
+$endereco = new Endereco('Cidade', 'Bairro', 'Rua', '100');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
-$primeiraConta->saca(300);
+$primeiraConta->saca(300); // 
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
+
+
+$outroEndereco = new Endereco ('AB', 'BB' ,'CC', 'DD');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
